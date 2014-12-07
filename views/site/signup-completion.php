48a10dd04f0016daccf29fd003e5bb5a42685d8e
RazvanGirmacea <girmacearazvan@gmail.com>
2014-12-07 22:47:25 +0200

    fix frontend bug on profile tweaked 2nd step after authentification

--- views/site/signup-completion.php
+++ views/site/signup-completion.php
@@ -5,13 +5,22 @@ use yii\helpers\Html;
 
 ?>
 
-<?php $form = ActiveForm::begin([
-    'id' => 'signup-completion',
-]) ?>
-<?= $form->field($user, 'email')->input('email') ?>
-<?= $form->field($user, 'first_name')->input('text') ?>
-<?= $form->field($user, 'last_name')->input('text') ?>
 
-<?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        <h2>Almost done ... </h2>
+        <div>
+            <?php $form = ActiveForm::begin([
+                    'id' => 'signup-completion',
+                ]) ?>
+            <?= $form->field($user, 'email')->input('email') ?>
+            <?= $form->field($user, 'first_name')->input('text') ?>
+            <?= $form->field($user, 'last_name')->input('text') ?>
 
-<?php ActiveForm::end() ?>
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+
+            <?php ActiveForm::end() ?>
+        </div>
+    </div>
+</div>
+
