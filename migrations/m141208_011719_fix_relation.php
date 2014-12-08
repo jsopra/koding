2dc5ffa14429cd7836f84d6440a881fa0e5c210a
armab <blog@arma.biz>
2014-12-08 04:24:34 +0200

    Fix event_user relation

--- migrations/m141208_011719_fix_relation.php
+++ migrations/m141208_011719_fix_relation.php
@@ -15,8 +15,6 @@ class m141208_011719_fix_relation extends Migration
      */
     public function up()
     {
-        $this->execute('DELETE E.* FROM `event_user` AS E LEFT JOIN `user` AS U ON E.user_id=U.id AND U.id IS NULL');
-        $this->execute('DELETE EU.* FROM `event_user` AS EU LEFT JOIN `event` AS E ON EU.user_id=E.id AND E.id IS NULL');
         $this->addForeignKey('event_user-user_id_fk', '{{%event_user}}', 'user_id', '{{%user}}', 'id', 'CASCADE', 'CASCADE');
         $this->addForeignKey('event_user-event_id_fk', '{{%event_user}}', 'event_id', '{{%event}}', 'id', 'CASCADE', 'CASCADE');
     }
